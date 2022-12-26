f2c9d66e049b103afa28bfd6751ebccd521f26e7
Vinoth1704 <vinoth.1704j@gmail.com>
2022-12-26 12:09:45 +0530

    added confirmation methos for direct exchange

--- Receive/Program.cs
+++ Receive/Program.cs
@@ -16,3 +16,5 @@ class Receive
         Console.ReadKey();
     }
 }
+
+
--- send/DirectExchange.cs
+++ send/DirectExchange.cs
@@ -8,15 +8,21 @@ class DirectExchange
         var factory = new ConnectionFactory() { HostName = "localHost" };
         var connection = factory.CreateConnection();
         var channel = connection.CreateModel();
+        channel.ConfirmSelect();
 
         //Direct Method
         channel.ExchangeDeclare(exchange: "Direct", type: ExchangeType.Direct);
         var message = "Direct Method working fine";
         var body = Encoding.UTF8.GetBytes(message);
-        channel.BasicPublish(exchange: "Direct",
+        var published = channel.BasicPublish(exchange: "Direct",
                             routingKey: "First",
                             basicProperties: null,
                             body: body);
+                            console.we
+        if (published)
+        {
+            channel.WaitForConfirmsOrDie();
+        }
         Console.WriteLine($"Sent \"{message}\" message to consumer");
 
 
