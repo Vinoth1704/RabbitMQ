fc6f20c5eca4545b9adf3c7fb07adb5e70e3e96e
Vinoth1704 <vinoth.1704j@gmail.com>
2023-02-08 11:23:19 +0530

    Added RPC pattern

--- send/Program.cs
+++ send/Program.cs
@@ -1,15 +1,15 @@
-﻿
-class Send
+﻿class Send
 {
     public static void Main()
     {
         // Basic.basic();
         // Work_Queue.workQueue();
-        DirectExchange.publish();
+        // DirectExchange.publish();
         // FanoutExchange.publish();
         // DeadLetterExchange.publish();
         // AlternateExchange.publish();
         // AcceptReject.publish();
+        RPC.RPCServer();
         Console.ReadKey();
     }
 }
